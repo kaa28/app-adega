10cb07b3c539eb98b9f152821df16f294eec5a71
kevin <ksbonancea@gmail.com>
2024-09-02 10:59:26 -0300

    O que foi feito

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Entypo, FontAwesome5, AntDesign  } from '@expo/vector-icons';
+
+import TelaInicial from "./components/TelaInicial";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <TelaInicial/>
   );
 }
 
